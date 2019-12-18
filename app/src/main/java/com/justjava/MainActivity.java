6f11239bae5b2ec9e793091f396a41d9aae557b6
deepak9962 <56271448+deepak9962@users.noreply.github.com>
2019-12-19 00:34:30 +0530

    Update MainActivity.java

--- app/src/main/java/com/justjava/MainActivity.java
+++ app/src/main/java/com/justjava/MainActivity.java
@@ -20,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+/*
+This function will be called when the order button clicked.
+*/
     public void submitOrder(View view) {
         int numberOfCoffees;
         numberOfCoffees = 3;
@@ -28,16 +31,26 @@ public class MainActivity extends AppCompatActivity {
         displayPrice(numberOfCoffees * 5);
     }
 
+/*
+This function will be called for which xml id will be change.
+*/
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_number);
         quantityTextView.setText("" + number);
     }
 
+/*
+This function will show the text "Order Submitted" in the bottom of screen
+*/
     private void displayorderSubmit(){
         TextView orderSubmittedTextView = (TextView) findViewById(R.id.order_submitted);
         orderSubmittedTextView.setText("Order Submitted");
     }
 
+/*
+This function will show the price with the local currency format
+For Example: For India it will be rupee symbol, like that for usa it will be dollar symbol.
+*/
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_number);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
